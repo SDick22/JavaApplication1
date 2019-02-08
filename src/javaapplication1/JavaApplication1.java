b466838b3dcdfc0c2e0f093a293c6d896fc42965
xXFry <xXFry@10.0.0.168>
2019-02-07 21:17:15 -0500

    Commenting

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,11 @@
+package javaapplication1;
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        SamDick();
+    }
+    public static void SamDick(){
+        System.out.println("It's me, Sam.");
+    }
+}
